a685d768fb612da9770ed6a3f1fa013c8f6d913e
ashrafsayed2018 <ashrafsayed19841111@gmail.com>
2025-07-02 21:03:07 +0300

    add health route in server

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,19 @@
+// api/index.ts
 const express = require('express')
+const { createServer } = require('http')
+
 const app = express()
 
-app.get('/', (req, res) => res.send('Express on Vercel'))
+app.get('/', (req, res) => {
+  res.send('Express running on Vercel! ashraf')
+})
 
-app.listen(3000, () => console.log('Server ready on port 3000.'))
+app.get("/health",(req,res) => {
+  res.send("this is the health route for my app")
+})
 
-module.exports = app
+const server = createServer(app)
+
+module.exports = (req: any, res: any) => {
+  server.emit('request', req, res)
+}
