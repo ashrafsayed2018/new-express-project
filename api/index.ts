0bf3483bc1172887f92628c79b992cd2b82da79a
ashrafsayed2018 <ashrafsayed19841111@gmail.com>
2025-07-02 21:28:00 +0300

    add api/hello 4 route in server

--- api/index.ts
+++ api/index.ts
@@ -8,10 +8,13 @@ app.get('/', (req, res) => {
   res.send('Express running on Vercel! ashraf')
 })
 
-app.get("/health",(req,res) => {
-  res.send("this is the health route for my app")
+app.get('/health', (req, res) => {
+  res.send('this is the health route for my app')
 })
 
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'Hello from the API!' })
+})
 const server = createServer(app)
 
 module.exports = (req: any, res: any) => {
